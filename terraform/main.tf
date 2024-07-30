45ffa80218a1d91a2f68c1e55b9e57a046dca0b2
james-knott-iw <james.knott@integration.works>
2024-07-31 11:00:10 +1200

    remove prevent destroy

--- terraform/main.tf
+++ terraform/main.tf
@@ -36,9 +36,7 @@ resource "azurerm_virtual_network" "this" {
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  
 }
 
 resource "azurerm_subnet" "psql" {
