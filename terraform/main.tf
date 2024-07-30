62485b15979ca936920e2a366b45be4815d46847
james-knott-iw <james.knott@integration.works>
2024-07-31 11:52:35 +1200

    dix references to resource group

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ data "azurerm_container_registry" "acr" {
   resource_group_name = "james-sandbox"
 }
 resource "azurerm_private_dns_zone" "this" {
-  depends_on = [ azurerm_resource_group.this ]
+  depends_on = [ data.azurerm_resource_group.this ]
   name                = var.private_dns_zone_name
   resource_group_name = var.resource_group_name
 }
@@ -36,7 +36,7 @@ resource "azurerm_role_assignment" "containerapp" {
 
 resource "azurerm_postgresql_flexible_server" "this" {
   name                = "${var.resource_prefix}-psql"
-  location            = azurerm_resource_group.this.location
+  location            = data.azurerm_resource_group.this.location
   resource_group_name = var.resource_group_name
 
   version                       = "16"
@@ -53,9 +53,9 @@ resource "azurerm_postgresql_flexible_server" "this" {
 }
 
 resource "azurerm_virtual_network" "this" {
-  depends_on = [ azurerm_resource_group.this ]
+  depends_on = [ data.azurerm_resource_group.this ]
   name                = "${var.resource_prefix}-vnet"
-  location            = azurerm_resource_group.this.location
+  location            = data.azurerm_resource_group.this.location
   resource_group_name = var.resource_group_name
   address_space       = ["10.0.0.0/16"]
 
@@ -65,7 +65,7 @@ resource "azurerm_virtual_network" "this" {
 resource "azurerm_subnet" "psql" {
   depends_on = [ azurerm_virtual_network.this ]
   name                 = var.psql_subnet_name
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = data.azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.0.0/24"]
 
@@ -74,16 +74,16 @@ resource "azurerm_subnet" "psql" {
 resource "azurerm_subnet" "aca-env" {
   depends_on = [ azurerm_virtual_network.this ]
   name                 = var.aca_env_subnet_name
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = data.azurerm_resource_group.this.name
   virtual_network_name = azurerm_virtual_network.this.name
   address_prefixes     = ["10.0.2.0/23"]
 }
 
 resource "azurerm_log_analytics_workspace" "this" {
-  depends_on = [ azurerm_resource_group.this ]
+  depends_on = [ data.azurerm_resource_group.this ]
   name                = "${var.resource_prefix}-logs"
-  location            = azurerm_resource_group.this.location
-  resource_group_name = azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
@@ -91,8 +91,8 @@ resource "azurerm_log_analytics_workspace" "this" {
 resource "azurerm_container_app_environment" "this" {
   depends_on = [ azurerm_subnet.aca-env ]
   name                       = "${var.resource_prefix}-aca-env"
-  location                   = azurerm_resource_group.this.location
-  resource_group_name        = azurerm_resource_group.this.name
+  location                   = data.azurerm_resource_group.this.location
+  resource_group_name        = data.azurerm_resource_group.this.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.this.id
   infrastructure_subnet_id   = azurerm_subnet.aca-env.id
 }
@@ -101,7 +101,7 @@ module "nginx" {
 
   source = "./modules/container-app"
 
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = data.azurerm_resource_group.this.name
   aca_name             = var.nginx_aca_name
   container_name       = var.nginx_container_name
   container_image_name = var.nginx_container_image_name
@@ -120,7 +120,7 @@ module "backstage" {
   depends_on = [azurerm_postgresql_flexible_server.this, azurerm_container_app_environment.this]
   source     = "./modules/container-app"
 
-  resource_group_name  = azurerm_resource_group.this.name
+  resource_group_name  = data.azurerm_resource_group.this.name
   aca_name             = var.backstage_aca_name
   container_name       = var.backstage_container_name
   container_image_name = var.backstage_container_image_name
