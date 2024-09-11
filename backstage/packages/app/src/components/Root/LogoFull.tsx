184a198b255ef8062d6ea1220708b71a7df7d824
james-knott-iw <james.knott@integration.works>
2024-09-11 12:08:15 +1200

    fix export

--- backstage/packages/app/src/components/Root/LogoFull.tsx
+++ backstage/packages/app/src/components/Root/LogoFull.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import MyCustomLogoFull from './logo/Gusion5_B3_Light.png';
 
-export const LogoFull = () => {
+const LogoFull = () => {
   return <img src={MyCustomLogoFull} />;
 };
+
+
+export default LogoFull
