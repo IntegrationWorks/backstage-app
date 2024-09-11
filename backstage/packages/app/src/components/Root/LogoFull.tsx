ff7b2af2dcfd4bf9757462bfc20416fee6e2234a
james-knott-iw <james.knott@integration.works>
2024-09-11 14:13:41 +1200

    modify logo width

--- backstage/packages/app/src/components/Root/LogoFull.tsx
+++ backstage/packages/app/src/components/Root/LogoFull.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import MyCustomLogoFull from './logo/Fusion5_B3_Light.png';
-
+import { makeStyles } from '@material-ui/core';
+const useStyles = makeStyles({
+  img: {
+    width: 'auto',
+    height: 30,
+  },
+  path: {
+    fill: '#7df3e1',
+  },
+});
 const LogoFull = () => {
-  return <img src={MyCustomLogoFull} />;
+  const classes = useStyles();
+  return <img className={classes.img} src={MyCustomLogoFull} />;
 };
 
 
