8ab4aad7d9e10dae4e998256f8a406ac56d2fcf5
james-knott-iw <james.knott@integration.works>
2024-09-19 10:18:49 +1200

    add search

--- backstage/packages/app/src/components/home/HomePage.tsx
+++ backstage/packages/app/src/components/home/HomePage.tsx
@@ -1,6 +1,17 @@
+import { Page } from '@backstage/core-components';
+import { TextField } from '@material-ui/core';
+
 import React from 'react';
 
 export const HomePage = () => (
     /* We will shortly compose a pretty homepage here. */
-    <h1>Welcome to Backstage!</h1>
+
+    <Page themeId='home'>
+        <h1>Welcome to Backstage!</h1>
+        <TextField>
+
+        </TextField>
+    </Page>
+
+
 );
